8a4c97650c2769b0372983f5b30b236364df8e57
Karolis Markevičius <karolis.markevicius@protonmail.ch>
2019-05-20 23:46:26 +0200

    additional packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
     entry: __dirname + '/src/index.js',
     module: {
@@ -6,6 +8,14 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
+            },
+            {
+            test: /\.css$/,
+                use: ['style-loader','css-loader', {
+                loader: 'postcss-loader',
+                options: {
+                    plugins: () => [require('autoprefixer')]
+                }}]
             }
         ]
     },
@@ -13,10 +23,20 @@ module.exports = {
         extensions: ['*', '.js', '.jsx']
     },
     output: {
-        path: __dirname + '/dist',
+        path: __dirname + '/dist/assets',
         publicPath: '/',
+        sourceMapFilename: "bundle.map",
         filename: 'bundle.js'
     },
+    plugins: [
+        // new webpack.optimize.UglifyJsPlugin({
+        //    sourceMap: true,
+        //    warnings: false,
+        //    mangle: true,
+        // }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devtool: "#source-map",
     devServer: {
         contentBase: './dist'
     }
