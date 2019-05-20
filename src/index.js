df51f2f34e193e27c76b2e94c77c82376f9313df
Karolis Markevičius <karolis.markevicius@protonmail.ch>
2019-05-20 22:12:22 +0200

    webpack/ babel / react installation

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const title = 'Minima React Webpack Babel setup';
+
+ReactDom.render(
+    <div>{title}</div>,
+    document.getElementById('app')
+);
